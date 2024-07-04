bf7a644ae85812cbde87ff5815b6ad8dbd12c5d9
Madn <madan.patakota@gmail.com>
2024-07-04 07:46:08 +0530

    updage

--- src/app/advancedcolor.directive.ts
+++ src/app/advancedcolor.directive.ts
@@ -29,8 +29,14 @@ export class AdvancedcolorDirective implements OnInit {
   @HostBinding("style.color")   hostbackgroundcolor : string;
 
   ngOnInit(): void {
-       //this.elementref.nativeElement.style.background = "yellow";  // you are applying the style by using HTML DOM 
 
+       //this.elementref.nativeElement.style.background = "yellow"; 
+        // you are applying the style by using HTML DOM directly. 
+        //so its not good approach direclty apply the styles by using the html dom.
+        // because XSS(Cross site script issues) problems will come
+
+
+      // Renderer2 is a service in Angular that provides a safe and universal way to manipulate the DOM. It helps ensure your code is secure and works consistently across different environments.
       //  this.render.setStyle(this.elementref.nativeElement , 'background-color', 'yellow');
 
 
